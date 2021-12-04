abe4148e34de6e36c67977b856806f40304262f2
holodovrpk <holodovrpk@gmail.com>
2021-12-04 19:46:00 +0200

    start project

---
+++ one.py
@@ -0,0 +1,15 @@
+
+
+N = int(input())
+a = input().split()
+if len(a) == N:
+    flag = True
+    for i in range(0, len(a)):
+        a[i] = int(a[i])
+        if a[i] < -99999 or a[i] > 99999:
+            flag = False
+    if flag:
+        max_number = max(a)
+        min_number = min(a)
+        s = max_number + min_number
+        print(s)
---
+++ two.py
@@ -0,0 +1,16 @@
+s = input().split()
+
+n = int(s[0])
+m = int(s[1])
+
+
+
+if n<10**22 and m < 10**22:
+    l = list(str(n + m))
+    summa = 0
+    for i in l:
+        summa += int(i)
+    print(summa)
+else:
+    print("нет")
+
